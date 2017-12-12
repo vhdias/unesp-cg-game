15779cff39e1f7599809dfbd672ee9e11ee3e9cf
Victor Hugo da Silva Dias <victorhugo.dias@uol.com.br>
2017-12-11 23:23:52 -0200

    Creating menu on test scene
    Also created scripts to support this.

---
+++ Assets/Scripts/CameraSwitchController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraSwitchController : MonoBehaviour
+{
+    public GameObject[] cameras;
+    public Text text;
+
+    private int m_CurrentActiveObject;
+
+
+    private void OnEnable()
+    {
+        text.text = cameras[m_CurrentActiveObject].name;
+    }
+
+
+    public void NextCamera()
+    {
+        int nextactiveobject = m_CurrentActiveObject++ % cameras.Length;
+
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            cameras[i].SetActive(i == nextactiveobject);
+        }
+
+        m_CurrentActiveObject = nextactiveobject;
+        text.text = cameras[m_CurrentActiveObject].name;
+    }
+}
+
---
+++ Assets/Scripts/SceneAndURLController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneAndURLController : MonoBehaviour {
+    private PauseMenu m_PauseMenu;
+
+    private void Awake()
+    {
+        m_PauseMenu = GetComponentInChildren<PauseMenu>();
+    }
+
+    public void SceneLoad(string sceneName)
+    {
+        //PauseMenu pauseMenu = (PauseMenu)FindObjectOfType(typeof(PauseMenu));
+        m_PauseMenu.MenuOff();
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("O jogo irá fechar (Não funciona no editor -- https://docs.unity3d.com/ScriptReference/Application.Quit.html)");
+        Application.Quit();
+    }
+}

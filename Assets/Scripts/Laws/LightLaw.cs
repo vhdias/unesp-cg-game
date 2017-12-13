912f01fd7193cad8337005a10c43568b32c0b37f
Victor Hugo da Silva Dias <victorhugo.dias@uol.com.br>
2017-12-13 21:20:06 -0200

    Insert light check

---
+++ Assets/Scripts/Laws/LightLaw.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DayTime
+{
+    Day,
+    Night
+};
+
+public class LightLaw : MonoBehaviour {
+    public GameObject lights;
+    public float timeToChangeLights = 5;
+    public DayTime dayTime; 
+    bool timeOk = false;
+
+    private void Awake()
+    {
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay()
+    {
+        timeOk = false;
+        yield return new WaitForSecondsRealtime(timeToChangeLights);
+        timeOk = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.transform.parent;
+        if (player)
+        {
+            player = player.parent;
+            if (player)
+            {
+                StartCoroutine(Delay());
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        var player = other.transform.parent;
+        if (player)
+        {
+            player = player.parent;
+            if (player && !StatusOk(lights.activeInHierarchy) && timeOk && player.gameObject.name == "Jogador(Carro)")
+            {
+                if (dayTime == DayTime.Day)
+                {
+                    Debug.Log("Recomenda-se desligar o farol");
+                }
+                else
+                {
+                    Debug.Log("Ligue o farol");
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.transform.parent;
+        if (player)
+        {
+            player = player.parent;
+            if (player && !lights.activeInHierarchy && player.gameObject.name == "Jogador(Carro)")
+            {
+                if (dayTime == DayTime.Night)
+                {
+                    Debug.Log("Desligue o farol");
+                }
+                else
+                {
+                    Debug.Log("Ligue o farol");
+                }
+            }
+        }
+    }
+
+    private bool StatusOk(bool lightOn)
+    {
+        return (dayTime == DayTime.Night && lightOn) || (dayTime == DayTime.Day && !lightOn);
+    }
+}

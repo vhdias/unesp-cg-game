29047ef115927f523e28402e981403d9ed192a3a
Victor Hugo da Silva Dias <victorhugo.dias@uol.com.br>
2017-12-13 19:21:54 -0200

    Control of speed and direction on car

---
+++ Assets/Scripts/Laws/SpeedLimitAndCarMoveBack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedLimitAndCarMoveBack : MonoBehaviour {
+    public float maxSpeed = 80, speedScale = 1, limiteRe = 5;
+    //Artigo 192 define quanto as restrições de marcha ré de forma subjetiva. Definimos 5 segundos.
+    public Text speedText;
+    private float timerRe;
+	
+	void FixedUpdate () {
+        float speed = VelocidadeKPH(GetComponent<Rigidbody>()) * speedScale;
+        bool re = Re(GetComponent<Rigidbody>());
+        if (re) timerRe += Time.deltaTime;
+        else timerRe = 0;
+        if(timerRe > limiteRe)
+        {
+            Debug.Log("Excedeu o tempo de ré" + timerRe); 
+        }
+        speedText.text = Mathf.RoundToInt(speed) + "Km/H" + ((re == true) ? "(Ré)":"");
+        if (speed > maxSpeed)
+        {
+            Debug.Log("Excedeu o limite de velocidade" + speed);
+        }
+	}
+
+    public static float VelocidadeKPH(Rigidbody body)
+    {
+        return body.velocity.magnitude * 3.6f;
+    }
+
+    public static bool Re(Rigidbody body)
+    {
+        return Mathf.RoundToInt(body.GetComponent<Transform>().InverseTransformVector(body.velocity).z) < 0;
+    }
+}
